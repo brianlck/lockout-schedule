2840c4a02c59b0f5e570ffc0a50ba56b0341a929
lckcode <brianlee.lck@gmail.com>
2021-07-06 03:44:07 +0800

    min cost max flow implemented

---
+++ src/flow.py
@@ -0,0 +1,115 @@
+from typing import ClassVar, List, Tuple, Union
+from queue import Queue
+import math
+
+CostType = Union[int, float]
+
+class Edge:
+    u: ClassVar(int)
+    v: ClassVar(int)
+    capacity: ClassVar(int)
+    cost: ClassVar(CostType)
+    backward_edge: ClassVar('Edge')
+
+    def __init__(self, u: int, v: int, capacity: int, cost: CostType, is_forward: bool = True) -> None:
+        """ Initialize edge from u to v with no flow """
+        self.u = u
+        self.v = v
+        self.capacity = capacity
+        self.cost = cost
+        self.is_forward = is_forward
+        self.flow = 0
+    
+    def get_residual_capacity(self) -> CostType:
+        return self.capacity - self.flow
+
+    def is_valid(self) -> bool:
+        return self.flow >= 0 and self.flow <= self.capacity
+
+    def set_backward_edge(self, backward_edge: 'Edge') -> None:
+        self.backward_edge = backward_edge
+
+    def use_edge(self, flow) -> CostType:
+        self.flow += flow
+        self.backward_edge.flow -= flow
+        assert self.is_valid()
+        assert self.backward_edge.is_valid()
+        return flow * self.capacity
+
+
+class FlowNetwork:
+    """ Flow Network implemented with adjacent list """
+    nodes: ClassVar(int) # number of nodes
+    adjacency_list: ClassVar(List[List['Edge']]) # Adjacency list of edges
+    ran: bool # whether min_cost_max_flow function is ran
+
+    def __init__(self, nodes) -> None:
+        self.nodes = nodes
+        self.adjacency_list = [[] for i in range(0, nodes)]
+        self.ran = False
+
+    def add_edge(self, u: int, v: int, capacity: int, cost: CostType) -> None:
+        """ Add edge from u to v """
+        assert u >= 0 and u <= self.nodes
+        assert v >= 0 and v <= self.nodes
+        assert capacity >= 0
+        forward_edge = Edge(u, v, capacity, cost, is_forward=True)
+        backward_edge = Edge(v, u, capacity, -cost, is_forward=False)
+        forward_edge.set_backward_edge(backward_edge)
+        backward_edge.set_backward_edge(forward_edge)
+        self.adjacency_list[u].append(forward_edge)
+        self.adjacency_list[v].append(backward_edge)
+    
+    def get_edges(self) -> List['Edge']:
+        return [edge for adjacency in self.adjacency_list for edge in adjacency]
+
+    def __shortest_path(self, source: int, sink: int) -> Union[bool, Tuple[int, CostType, List[Edge]]]:
+        assert source >= 0 and source <= self.nodes
+        assert sink >= 0 and sink <= self.nodes
+        distance = [math.inf] * self.nodes
+        parent = [None] * self.nodes
+        in_queue = [False] * self.nodes
+        spfa_queue = Queue(maxsize=self.nodes)
+
+        spfa_queue.put(source)
+        distance[source] = 0
+        while not spfa_queue.empty():
+            u = spfa_queue.get()
+            in_queue[u] = False
+            for edge in self.adjacency_list[u]:
+                if not in_queue[edge.v] and edge.flow < edge.capacity and distance[edge.v] + edge.cost < distance[edge.v]:
+                    spfa_queue.put(edge.v)
+                    in_queue[edge.v] = True
+                    parent[edge.v] = edge
+
+        if distance[sink] == math.inf:
+            # no path exists
+            return False
+        
+        max_flow = parent[sink].get_residual_capacity()
+        current_node = sink
+        path_edges = []
+        while current_node != source:
+            max_flow = min(
+                max_flow, parent[current_node].get_residual_capacity())
+            path_edges.append(parent[edge.v])
+            current_node = parent[edge.v].u
+        return max_flow, distance[sink], path_edges
+
+    def min_cost_max_flow(self, source: int, sink: int) -> CostType:
+        assert source >= 0 and source <= self.nodes
+        assert sink >= 0 and sink <= self.nodes
+        assert self.ran == False
+        max_flow: int = 0
+        min_cost: CostType = 0
+        while True:
+            result = self.__shortest_path(source, sink)
+            if result == False:
+                # No path exists
+                break
+            flow, distance, edges = result
+            max_flow += flow * distance 
+            for edge in edges:
+                min_cost += edge.useEdge(flow)
+        self.ran = True
+        return max_flow, min_cost
